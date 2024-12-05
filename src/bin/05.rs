04df34b07d555e1b9df87973ebd48247ec7b31e9
meeeteor <teormee@gmail.com>
2024-12-05 16:24:26 +0100

    Solved day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use itertools::Itertools;
 
-// use itertools::Itertools;
 advent_of_code::solution!(5);
 
 
@@ -28,36 +27,50 @@ pub fn part_one(input: &str) -> Option<u32> {
 
 pub fn part_two(input: &str) -> Option<u32> {
     let (rules, updates) = interpret_input(input);
-    let rulemap: HashMap<usize, usize> = rules.iter().collect();
+    let mut rule_map = HashMap::new();
+    rules.iter().for_each(|(a,b)| rule_map.entry(a).or_insert_with(Vec::new).push(b));
 
-    let x:i32 = updates.iter().map(|update| {
-        // map page to position in update pages
-        let positions: HashMap<usize, usize> = update.iter()
-            .enumerate()
-            .map(|(i, &v)| (v, i)).collect();
+    let invalid_updates = updates.iter()
+        .filter(|update| update.iter()
+            .tuple_combinations()
+            .any(|(a, b)| {
+                match rule_map.get(b) {
+                    Some(pages) => pages.contains(  &a),
+                    None => false
+                }
+            }))
+        .collect::<Vec<_>>();
 
-        // check whether update is valid
-        let valid_update = rules.iter()
-            .all(|&(node_a, node_b)| {
-                !positions.contains_key(&node_a) || !positions.contains_key(&node_b) || positions[&node_a] < positions[&node_b]
-            });
+    let sum: i32 = invalid_updates.iter()
+        .map(|&update| {
+            let mut update = update.clone();
+            let mut swapped = true;
+            while swapped {
+                swapped = false;
+                let mut new_update = update.clone();
+                update.iter().enumerate().tuple_combinations().for_each(|((a_idx, a_val), (b_idx, b_val))| {
+                    match rule_map.get(b_val) {
+                        Some(rule) => {
+                            if rule.contains(&a_val) {
+                                new_update.swap(a_idx, b_idx);
+                                swapped=true;
+                            }
+                        }
+                        None => {}
+                    }
+                    // if let mut rule = rule_map.get(b_val) {
+                    //     if rule.iter().contains(a_val) {
+                    //         new_update.swap(*a_idx, b_idx);
+                    //         swapped=true;
+                    //     }
+                    // }
+                });
+                update = new_update;
+            }
+            update[update.len() / 2] as i32
+        }).sum();
 
-        if !valid_update {
-            println!("{:?}", update);
-
-            let mut new_update = update.clone();
-            new_update.iter().for_each(|page| {
-
-            });
-
-            println!("{:?}", update);
-        }
-
-        // update[update.len() / 2] as i32
-        0
-    }).sum();
-
-    Some(x as u32)
+    Some(sum as u32)
 }
 
 fn interpret_input(input: &str) -> (Vec<(usize, usize)>, Vec<Vec<usize>>) {
